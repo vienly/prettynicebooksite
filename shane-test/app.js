81644f8d3fe1ddd2d26a53e6896728d513046eed
Shane Henning <shanerhenning@gmail.com>
2016-05-30 19:06:32 -0700

    handlebarred that mother

--- shane-test/app.js
+++ shane-test/app.js
@@ -1,13 +1,16 @@
-// var ret;
-
 function Input (opts) {
   for (keys in opts) {
     this[keys] = opts[keys];
   };
 }
 
+function Output (opts) {
+  for (keys in opts) {
+    this[keys] = opts[keys];
+  };
+}
+
 var formInput;
-// var Input = {};
 newInput = function(){
   formInput = new Input({
     keyword:        $('#keyword').val().replace(/\W+/g, '+'),
@@ -16,6 +19,31 @@ newInput = function(){
     genre:          $('#genre').val(),
     publisher:      $('#publisher').val(),
     isbn:           $('#isbn').val()
+  });
+};
+
+Output.prototype.renderResults = function(){
+  var template = Handlebars.compile($('#results-template').text());
+  return template(this);
+};
+
+app = function(dataOutput){
+  $('#results').append(dataOutput.renderResults($('#results-template')));
+};
+
+var view = {};
+view.getInfo = function(data){
+  data.forEach(function(item){
+    item = new Output({
+      bookTitle:    item.volumeInfo.title,
+      thumb:        item.volumeInfo.imageLinks.thumbnail,
+      author:       item.volumeInfo.authors,
+      genre:        item.volumeInfo.category,
+      publisher:    item.volumeInfo.publisher,
+      isbn:         item.volumeInfo.categories,
+      description:  item.volumeInfo.description
+    });
+    $('#results').append(item.renderResults());
   });
 };
 
@@ -42,68 +70,63 @@ var createEndpoint = function(){
   return newUrl;
 };
 
-// var keywordInput = $('#keyword').val();
-// var bookTitleInput = $('book-title').val().replace(/\W+/g, '+');
-// var authorInput = $('author').val().replace(/\W+/g, '+');
-// var genreInput = $('genre').val().replace(/\W+/g, '+');
-// var publisherInput = $('publisher').val().replace(/\W+/g, '+');
-// var isbnInput = $('isbn').val().replace(/\W+/g, '+');
 var retData;
 ajaxCall = function(e){
   e.preventDefault();
   $('#results').empty();
   var endpoint = createEndpoint();
-  console.log(endpoint);
-  console.log(formInput.keyword);
+  // console.log(endpoint);
+  // console.log(formInput.keyword);
   $.ajax({
     url: endpoint
     // 'https://www.googleapis.com/books/v1/volumes' +
     // ?q=wise+inauthor:rothfuss&?key=AIzaSyCfsM3QeTqrabiuQ1f97bB7pawjROuhhv0',
-    // '?q=' +
-    // formInput.keyword + //keyword
-    // '+' +
-    // 'inauthor:' +
-    // 'rowling' + //author
-    // '+' +
-    // 'intitle:' +
-    // 'wise' +
-    // '+' +
-    // 'inpublisher:' +
-    // 'penguin' +
-    // '+' +
-    // 'subject:' +
-    // 'fiction' +
-    // '+' +
-    // 'isbn:' +
-    // '9781101486405' +
     + '&maxResults=40' +
-    // '&startIndex=30' +
     '&?key=AIzaSyCfsM3QeTqrabiuQ1f97bB7pawjROuhhv0',
 
     type: 'GET',
     success: function(data) {
-      console.log(data.items);
+      // console.log(data.items);
       retData = data;
-      var filteredArray = [];
-      var mostFrequentAuth = mostFrequent(data.items);
-      console.log(mostFrequentAuth);
+      // var filteredArray = [];
+      var mostFrequentPub = mostFrequent(data.items);
+      // console.log('most frequent publisher: ' + mostFrequentPub);
       data.items.map(function(item){
-        console.log(item.volumeInfo.title);
+        // console.log(item.volumeInfo.title);
       });
       var ret = data.items.filter(function(item){
-        if(item.volumeInfo.ratingsCount > 10 && item.accessInfo.country === 'US' && item.volumeInfo.language == 'en'){
+        if(item.volumeInfo.ratingsCount > 10 && item.accessInfo.country === 'US' && item.volumeInfo.language == 'en' && item.volumeInfo.imageLinks.thumbnail){
           return item;
         }
       });
+      // .reduce(function(a,b){
+      //   // console.log('a: ' + a.volumeInfo.title);
+      //   // console.log('b: ' + b.volumeInfo.title);
+      //   if (a.indexOf(b.volumeInfo.title) < 0 ) a.push(b);
+      //   // console.log('a: ' + a);
+      //   return a;
+      // },[]);
+      //
+      // .reduce(function(a, arr){
+      //   if (arr.length > 0){
+      //     return arr.indexOf(a) !== -1;
+      //   }
+      //   console.log(arr);
+      // }, []);
       // .filter(function(item) {
       //   return item.volumeInfo.publisher && item.volumeInfo.publisher === (mostFrequentPub.toString());
       // })
       // .sort((function(a,b){
       //   return b.volumeInfo.publishedDate > a.volumeInfo.publishedDate;
       // }));
-      $('#results').html(ret.map(function(item){
-        return item.volumeInfo.title + '</br>';
-      }));
+      // ret.forEach(function(item){
+      //   var output = new Output(item);
+      //   $('#results').append(renderResults(output));
+      // });
+      view.getInfo(ret);
+      // $('#results').html(ret.map(function(item){
+      //   return item.volumeInfo.title + '</br>';
+      // }));
     }
   }
 );
