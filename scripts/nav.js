ffc5ea562c8d125912e738cc6a1c0128eb1b2523
Gin <vienlyt@gmail.com>
2016-05-31 11:10:20 -0700

    fleshed out layout

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   console.log('script running');
   var $navButton = $('.nav-toggle-button');
+  var $siteWrapper = $('.site-wrap');
   $navButton.on('click', function() {
-    $(this).toggleClass('active');
+    $(this).toggleClass('navActive');
+    $siteWrapper.toggleClass('navActive');
     console.log($(this).attr('class'));
+
   });
 });
